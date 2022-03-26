bc06ec59ced80079e3c2377b387f7235947d2724
Alexander Werner <a.werner27@gmx.de>
2022-03-26 13:58:36 +0100

    Docker und MongoDB

---
+++ backend/source/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import movieRoutes from "./routes/movieRoutes.js";
+import mongoose from "mongoose";
+
+// complete application is here
+const app = express();
+const port = 4000;
+
+
+app.use(bodyParser.json());
+
+//routes are /movies & everything else throws a 404
+app.use("/movies", movieRoutes);
+app.all("*", (req, res) => res.sendStatus(404));
+
+
+
+
+mongoose.connect("mongodb://127.0.0.1/testdb").then(() => {
+console.log("Database connected");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on: http://localhost:${port}`);
+  
+});
