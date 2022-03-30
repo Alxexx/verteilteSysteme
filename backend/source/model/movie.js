c7f6c48bb6322b993c4f2d82cd9d3f613c5fb6a5
Alexander Werner <a.werner27@gmx.de>
2022-03-30 09:42:48 +0200

    id auskommentiert

---
+++ backend/source/model/movie.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema({
+  //id: Number,
+  title: String,
+  director: String,
+  duration: Number,
+  
+});
+
+export const Movie = mongoose.model("Movie", movieSchema);
